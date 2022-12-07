895240db1203890a320bd001f9c8f6e542ea44dc
essayZW <48241531+essayZW@users.noreply.github.com>
2022-12-07 21:33:58 +0800

    Update nvim-treesitter.lua

--- lua/plugin-config/nvim-treesitter.lua
+++ lua/plugin-config/nvim-treesitter.lua
@@ -1,18 +1,38 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"html", "css", "vim", "lua", "javascript", "typescript", "tsx"},
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false
-  },
-  -- 启用增量选择
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      node_incremental = '<CR>',
-      node_decremental = '<BS>',
-    }
-  },
-}
+require("nvim-treesitter.configs").setup({
+	ensure_installed = {
+		"html",
+		"css",
+		"vim",
+		"lua",
+		"javascript",
+		"typescript",
+		"go",
+		"vue",
+		"c",
+		"cpp",
+		"java",
+		"php",
+		"json",
+		"json5",
+		"jsonc",
+		"python",
+		"bash",
+		"yaml",
+	},
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+		disable = { "go" },
+	},
+	-- 启用增量选择
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			node_incremental = "<CR>",
+			node_decremental = "<BS>",
+		},
+	},
+})
 -- 开启 Folding
-vim.wo.foldmethod = 'expr'
-vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
